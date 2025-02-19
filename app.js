a33ac4885f4fd91f0747fca2cad89d79b23d2965
Laertis Vaso <laertis.vaso@gmail.com>
2025-02-19 13:51:45 +0100

    Add playRound() which plays a round of the game

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+
 /*
 Gets the computer's choice via a random number
 */
 function getComputerChoice() {
-    var randomNumber = Math.floor(Math.random() * 3);
+    const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
         return 'rock';
@@ -17,9 +20,33 @@ function getComputerChoice() {
 Gets the human's choice via a prompt. If the human enters an invalid choice, the function will prompt the human to enter a valid choice.
 */
 function getHumanChoice() {
-    var humanChoice = prompt('Please choose rock, paper, or scissors.').toLowerCase(); // Converts the human's choice to lowercase, so the function can validate the choice regardless of case.
+    let humanChoice = prompt('Please choose rock, paper, or scissors.').toLowerCase(); // Converts the human's choice to lowercase, so the function can validate the choice regardless of case.
     while (humanChoice !== 'rock' && humanChoice !== 'paper' && humanChoice !== 'scissors') {
         humanChoice = prompt('Invalid choice. Please choose rock, paper, or scissors.').toLowerCase();
     } // The while loop will continue to prompt the human to enter a valid choice until the human enters a valid choice.
     return humanChoice;
+}
+
+/*
+Capitalizes the first letter of a string.
+*/
+function capitalizeFirstLetter(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
+/*
+Plays a round of rock, paper, scissors. The function will increment the human's score if the human wins, the computer's score if the computer wins, or neither score if it's a tie.
+*/
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    if (humanChoice === computerChoice) {
+        console.log('It\'s a tie!');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore++;
+        console.log('You win! ' + capitalizeFirstLetter(humanChoice) + ' beats ' + computerChoice + '.');
+    } else {
+        computerScore++;
+        console.log('You lose! ' + capitalizeFirstLetter(computerChoice) + ' beats ' + humanChoice + '.');
+    }
 }
