d6ac11740d451f1ae9865b76bd1b0d586261f2eb
Laertis Vaso <laertis.vaso@gmail.com>
2025-02-19 13:40:09 +0100

    Add getHumanChoice to get the human choice

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/*
+Gets the computer's choice via a random number
+*/
 function getComputerChoice() {
     var randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -8,4 +11,15 @@ function getComputerChoice() {
         case 2:
         return 'scissors';
     }
-    }
+}
+
+/*
+Gets the human's choice via a prompt. If the human enters an invalid choice, the function will prompt the human to enter a valid choice.
+*/
+function getHumanChoice() {
+    var humanChoice = prompt('Please choose rock, paper, or scissors.').toLowerCase(); // Converts the human's choice to lowercase, so the function can validate the choice regardless of case.
+    while (humanChoice !== 'rock' && humanChoice !== 'paper' && humanChoice !== 'scissors') {
+        humanChoice = prompt('Invalid choice. Please choose rock, paper, or scissors.').toLowerCase();
+    } // The while loop will continue to prompt the human to enter a valid choice until the human enters a valid choice.
+    return humanChoice;
+}
